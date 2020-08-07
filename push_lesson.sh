7c8ab06d509e3a38baa36398f11dea4548f38df9
est2mzd <est2mzd@gmail.com>
2020-08-07 15:19:33 +0900

    main_13

--- push_lesson.sh
+++ push_lesson.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
+if [ $# = 0 ]; then
+   comments="update lesson files"
+else
+   comments="$1"
+fi
+
 git add .
-git commit -m "update lesson files"
+git commit -m "$comments"
 git push -u origin master
+
