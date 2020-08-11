408f27d0167b994ab25c4de5c437e9cfe0bdb129
est2mzd <est2mzd@gmail.com>
2020-08-11 11:50:04 +0900

    update lesson files

---
+++ Lessons/Lesson_03/main_04_example_04_quiz.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <thread>
+#include <mutex>
+
+std::mutex mtx_1, mtx_2;
+
+void Thread_A()
+{
+    // create deadlock problem
+    mtx_1.lock();
+    std::cout << "Thread A" << std::endl;
+    mtx_2.lock();
+    mtx_2.unlock();
+    mtx_1.unlock();
+}
+
+void Thread_B()
+{
+    // create deadlock problem
+    mtx_1.lock();
+    std::cout << "Thread B" << std::endl;
+    mtx_2.lock();
+    mtx_2.unlock();
+    mtx_1.unlock();
+}
+
+void ExecuteThreads()
+{
+    std::thread t1( Thread_A );
+    std::thread t2( Thread_B );
+    t1.join();
+    t2.join();
+
+    std::cout << "Finished" << std::endl;
+}
+
+int main()
+{
+    ExecuteThreads();
+    return 0;
+}
